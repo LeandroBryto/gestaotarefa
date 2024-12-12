87f6d068d1cc3493a3e8bcb5ae2d501da758739e
leandro barreto <leandrobarreto.barreto321@gmail.com>
2024-12-12 19:06:14 -0300

    Estrutura inicial

---
+++ src/test/java/br/com/gestaotarefa/colaborativo/GestaotarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestaotarefa.colaborativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaotarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
